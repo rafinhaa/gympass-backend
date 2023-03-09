4aca3539e1c078620d7018b0723564fd12772cc2
Rafinhaa <rafinhaa@me.com>
2023-03-09 17:45:12 -0300

    Caso de uso e design patterns - Criação de um usúario

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import fastify from "fastify";
 import { env } from "@/env";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 const envToLogger = {
   development: {
@@ -17,4 +19,24 @@ const envToLogger = {
 
 export const app = fastify({
   logger: envToLogger[env.NODE_ENV],
+});
+
+app.post("/users", async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, name, password } = registerBodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+
+  return reply.status(201).send();
 });
