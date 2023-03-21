ca8be3d9f56a2e4c9a75733a58337faae6b737eb
Rafinhaa <rafinhaa@me.com>
2023-03-21 18:14:52 -0300

    Controllers & Testes E2E - Teste E2E da autenticação

---
+++ src/http/controller/__tests__/authenticate.spec.ts
@@ -0,0 +1,30 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "@/app";
+import request from "supertest";
+
+describe("Authenticate E2E", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    await request(app.server).post("/users").send({
+      name: "John doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
