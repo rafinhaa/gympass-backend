7e4a9a9839eae42a3eee2b260c5e389a51bfe1e1
Rafinhaa <rafinhaa@me.com>
2023-03-21 18:02:21 -0300

    Controllers & Testes E2E - Teste E2E do registro

--- src/http/controller/__tests__/register.spec.ts
+++ src/http/controller/__tests__/register.spec.ts
@@ -1,5 +1,22 @@
-import { describe, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "@/app";
+import request from "supertest";
 
-describe("Register", () => {
-  it("should register", () => {});
+describe("Register E2E", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "John doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
 });
